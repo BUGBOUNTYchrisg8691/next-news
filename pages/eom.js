033c5cd07c04a672e016bcb38bc2d6ec431634da
Christopher Girvin <chrisg8691@gmail.com>
2021-05-06 10:03:22 -0400

    feat(eom.js): added headers to eom page for static site imformation

--- pages/eom.js
+++ pages/eom.js
@@ -1,21 +1,43 @@
+import { Head } from "next/head";
 import { Toolbar } from "../components/toolbar";
 import styles from "../styles/EOM.module.css";
 
 const EOM = ({ employee }) => {
   return (
-    <div className="page-container">
-      <Toolbar />
-      <div className={styles.main}>
-        <h1>Employee of the Month</h1>
+    <>
+      <Head>
+        <title>Employee of the Month</title>
+        <meta
+          name="description"
+          content={`This month's employee of the month is ${employee.month}`}
+        />
+        <meta property="og:image" content={employee.image} />
+        <meta property="og:title" content="Employee of the Month" />
+        <meta
+          property="og:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+        <meta property="twitter:image" content={employee.image} />
+        <meta property="twitter:title" content="Employee of the Month" />
+        <meta
+          property="twitter:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+      </Head>
+      <div className="page-container">
+        <Toolbar />
+        <div className={styles.main}>
+          <h1>Employee of the Month</h1>
 
-        <div className={styles.eom}>
-          <h3>{employee.name}</h3>
-          <h6>{employee.position}</h6>
-          <img src={employee.image} alt="employee of the month photo" />
-          <p>{employee.description}</p>
+          <div className={styles.eom}>
+            <h3>{employee.name}</h3>
+            <h6>{employee.position}</h6>
+            <img src={employee.image} alt="employee of the month photo" />
+            <p>{employee.description}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
